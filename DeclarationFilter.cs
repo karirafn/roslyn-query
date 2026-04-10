a79ce6f73be8bdf98c63eeec40c33e0eab28aa44
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-10 12:11:44 +0000

    feat: filter declaration sites from find-refs output
    Skip reference locations that match the symbol's own declaration
    by comparing SyntaxTree and TextSpan. Extracted DeclarationFilter
    helper for testability.

---
+++ DeclarationFilter.cs
@@ -0,0 +1,28 @@
+using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+public static class DeclarationFilter
+{
+    public static bool IsDeclarationSite(
+        SyntaxTree? referenceTree,
+        TextSpan referenceSpan,
+        ImmutableArray<Location> declarationLocations)
+    {
+        if (referenceTree is null)
+        {
+            return false;
+        }
+
+        foreach (Location declaration in declarationLocations)
+        {
+            if (declaration.SourceTree == referenceTree && declaration.SourceSpan == referenceSpan)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
